6774d543f197343099adeb95491218f78448a1dd
emrepun <emrehavan@hotmail.com>
2019-07-22 22:26:25 +0100

    Json conversion method applied for Option class.

--- version2/travel_app2/lib/option.dart
+++ version2/travel_app2/lib/option.dart
@@ -5,4 +5,15 @@ class Option {
 
   //constructor.
   Option(this.title, this.imageName, this.keywords);
+
+  Map<String, dynamic> optionToJson(bool isKeywordOnly) {
+    if (isKeywordOnly) {
+      return <String, dynamic> {'keywords': this.keywords};
+    } else {
+      return <String, dynamic> {
+        'title': this.title,
+        'imageName': this.imageName,
+        'keywords': this.keywords};
+    }
+  }
 }
