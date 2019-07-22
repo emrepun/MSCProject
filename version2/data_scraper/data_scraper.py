3c97b83e2f32cf20039c8b047e33e4ecf281cd15
emrepun <emrehavan@hotmail.com>
2019-07-22 17:41:33 +0100

    Writing scraped data to csv implemented.

--- version2/data_scraper/data_scraper.py
+++ version2/data_scraper/data_scraper.py
@@ -14,42 +14,65 @@ argumentArray = {'London', 'Istanbul', 'Amsterdam', 'Prague', 'Paris', 'Berlin',
 
 #https://www.tripadvisor.co.uk/Search?geo=1&searchNearby=&pid=3826&redirect=&startTime=1563560694952&uiOrigin=MASTHEAD&q=London&supportedSearchTypes=find_near_stand_alone_query&enableNearPage=true&returnTo=https%253A__2F____2F__www__2E__tripadvisor__2E__co__2E__uk__2F__Home__2D__g1&searchSessionId=AC98737AF729E0BC4B32BDFD46A864861563560612163ssid&social_typeahead_2018_feature=true&sid=AC98737AF729E0BC4B32BDFD46A864861563560698089&rf=1
 
+
 firstPartLink = "https://www.tripadvisor.co.uk/Search?geo=1&searchNearby=&pid=3826&redirect=&startTime=1563560694952&uiOrigin=MASTHEAD&q="
 secondPartLink = "&supportedSearchTypes=find_near_stand_alone_query&enableNearPage=true&returnTo=https%253A__2F____2F__www__2E__tripadvisor__2E__co__2E__uk__2F__Home__2D__g1&searchSessionId=AC98737AF729E0BC4B32BDFD46A864861563560612163ssid&social_typeahead_2018_feature=true&sid=AC98737AF729E0BC4B32BDFD46A864861563560698089&rf=1"
 
 browser = webdriver.Chrome()
 
-for i in argumentArray:
-    link = firstPartLink + i + secondPartLink
+with open('richCityData.csv', 'w') as csv_file:
+    csv_writer = writer(csv_file)
+    headers = ['city', 'popularity', 'description', 'image']
+    csv_writer.writerow(headers) # write headers first.
 
-    browser.get(link)
+    for i in argumentArray:
 
-    sleep(5)
+        link = firstPartLink + i + secondPartLink
+        browser.get(link)
+        sleep(5)
 
-    title = browser.find_elements_by_xpath("//div[@class='result-title']")
-    popularityElement = browser.find_elements_by_xpath("//div[@class='review-count']")
+        title = browser.find_elements_by_xpath("//div[@class='result-title']")
+        popularityElement = browser.find_elements_by_xpath("//div[@class='review-count']")
 
-    print(title[0].text)
-    print(popularityElement[0].text)
+        city = ''
+        popularity = ''
+        descToWrite = ''
+        imgUrl = ''
 
-    if not title:
-        continue
+        if title:
+            city = title[0].text
+        else:
+            continue
 
-    title[0].click()
-    sleep(2)
+        if popularityElement:
+            popularity = popularityElement[0].text
 
-    root_window = browser.window_handles[0]
-    opened_window = browser.window_handles[1]
+        title[0].click()
+        sleep(2)
 
-    browser.switch_to.window(opened_window)
-    sleep(5)
+        root_window = browser.window_handles[0]
+        opened_window = browser.window_handles[1]
 
-    description = browser.find_elements_by_xpath("//div[@class='common-text-ReadMore__content--2X4LR']")
-    imgElement = browser.find_elements_by_xpath("//img[@class='home_img']")
+        browser.switch_to.window(opened_window)
+        sleep(5)
 
-    print(description[0].text)
-    print(imgElement[0].text)
+        description = browser.find_elements_by_xpath("//div[@class='common-text-ReadMore__content--2X4LR']")
 
-    browser.close()
-    sleep(2)
-    browser.switch_to.window(root_window)    
+        if description:
+            descToWrite = description[0].text
+
+        imgElement = browser.find_elements_by_xpath("//img[@class='home_img']")
+
+        if imgElement:
+            imgUrl = imgElement[0].get_attribute("src")
+        else:
+            secondAttemptElement = browser.find_elements_by_xpath("//div[@class='homepage-homepage-hero-HomepageHero__hero--2vsJc']")
+            if secondAttemptElement:
+                url = secondAttemptElement[0].get_attribute("style")
+                imgUrl = url.split('(', 1)[1].split(')')[0]
+
+        browser.close()
+        sleep(2)
+        browser.switch_to.window(root_window)
+
+        csv_writer.writerow([city, popularity, descToWrite, imgUrl])
