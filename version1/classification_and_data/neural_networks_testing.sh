ee7739ab1b6690d32ac52a238f0ca3a1628c8d63
emrepun <emrehavan@hotmail.com>
2019-07-29 23:18:56 +0100

    neural networks testing bash completed.

--- version1/classification_and_data/neural_networks_testing.sh
+++ version1/classification_and_data/neural_networks_testing.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 
 echo initialized.
+
+#for sample_count in 500 1000 5000 10000 50000 100000 250000 500000 1000000 2500000 5000000
+for sample_count in 500 1000
+do
+  python3 header_neural_networks.py $sample_count
+  for i in {1..10}
+  do
+    python3 DataGenerator.py $sample_count
+    python3 TensorflowKerasClassifier.py
+  done
+
+  echo done with $sample_count
+done
+
+python3 footer_neural_networks.py
