36e6efbb126b210fa2580a1edb40d4914dadf0b4
emrepun <emrehavan@hotmail.com>
2019-08-13 23:34:02 +0100

    Rating multiplier with respect to quantity and threshold implemented.

--- version2/server/rating_extractor.py
+++ version2/server/rating_extractor.py
@@ -1,3 +1,5 @@
+from math import e
+
 class RatingExtractor:
     def __init__(self):
         print("rating initialized")
@@ -14,3 +16,19 @@ class RatingExtractor:
             m = (2*q) / 10 #10 because rating varies between 0 and 10
             b = -q
             return (m*rating) + b
+
+    @staticmethod
+    def get_rating_weight_with_quantity(rating, c, T, q=10):
+        if rating > 10 or rating < 0:
+            return None
+        else:
+            m = (2*q) / 10 #10 because rating varies between 0 and 10
+            b = -q
+            val = (m*rating) + b
+
+            M = e**((-T*0.68)/c)
+
+            return val * M
+
+#print(RatingExtractor.get_rating_weight_with_quantity(rating = 8.5, c = 1000, T = 100 ,q = 10))
+#print(RatingExtractor.get_rating_weight_no_quantity(rating = 8.5, q = 10))
